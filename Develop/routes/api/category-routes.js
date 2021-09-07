215c46ddb1136486ad5683f6f2ce7253cc4824fb
Lee <leenock2012@yahoo.co.uk>
2021-09-07 11:52:39 +0100

    Nearly there

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Products
   try {
     const categoryData = await Category.findAll({
-      include: [{ model: product }],
+      include: [{ model: Category }],
     });
     res.status(200).json(categoryData);
   } catch (err) {
