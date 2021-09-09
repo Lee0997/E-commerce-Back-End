33dc00848639f36787e2c894973ba8c44d0a3ded
Lee <leenock2012@yahoo.co.uk>
2021-09-09 20:26:11 +0100

    Work now

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,7 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    product_id: {
+    id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
@@ -30,9 +30,10 @@ Product.init(
     },
     category_id: {
       type: DataTypes.INTEGER,
+      allowNull: true,
       references: {
         model: 'Category',
-        key: 'category_id',
+        key: 'id',
         },
     },
   },
